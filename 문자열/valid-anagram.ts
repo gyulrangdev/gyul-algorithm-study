af79ba093a8f541004f5cd5abad1e5eee395124f
gyulrangdev <gyulrangdev@gmail.com>
2025-04-08 23:01:18 +0900

    Fix: Valid anagram

--- 문자열/valid-anagram.ts
+++ 문자열/valid-anagram.ts
@@ -1,3 +1,14 @@
+/**
+ *시간복잡도: O(n)
+ * s 문자열을 순회: O(n)
+ * t 문자열을 순회: O(n)
+ * Map의 get, set, has, delete 연산은 O(1)
+ * 총 O(n + n) = O(n)
+ *
+ *공간복잡도: O(n)
+ * charsMap 크기: O(n)
+ * 총 O(n)
+ */
 function isAnagram(s: string, t: string): boolean {
   if (s.length !== t.length) {
     return false;
@@ -25,32 +36,39 @@ function isAnagram(s: string, t: string): boolean {
   return charsMap.size === 0;
 }
 
+/**
+ * 시간복잡도: O(n)
+ * s 문자열을 순회: O(n)
+ * t 문자열을 순회: O(n)
+ * Map의 get, set, has, delete 연산은 O(1)
+ * 총 O(n + n) = O(n)
+ *
+ * 공간복잡도: O(n)
+ * map 크기: O(n)
+ * 총 O(n)
+ */
 function isAnagram2(s: string, t: string): boolean {
+  if (s.length !== t.length) return false;
+
   const map = new Map<string, number>();
 
   for (let i = 0; i < s.length; i++) {
-    if (map.has(s[i])) {
-      map.set(s[i], (map.get(s[i]) ?? 0) + 1);
-    } else {
-      map.set(s[i], 1);
-    }
+    map.set(s[i], (map.get(s[i]) ?? 0) + 1);
   }
 
   for (let i = 0; i < t.length; i++) {
+    if (!map.has(t[i])) {
+      return false;
+    }
+
     if (map.has(t[i])) {
       if ((map.get(t[i]) ?? 0) - 1 === 0) {
         map.delete(t[i]);
       } else {
         map.set(t[i], (map.get(t[i]) ?? 0) - 1);
       }
-    } else {
-      return false;
     }
   }
 
-  if (map.size === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }

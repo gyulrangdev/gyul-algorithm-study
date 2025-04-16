913324411d61e6360e45ed1b7b9d58cd57743026
gyulrangdev <gyulrangdev@gmail.com>
2025-04-16 21:42:53 +0900

    Docs: 주석 추가

--- 이진탐색/binary-search.ts
+++ 이진탐색/binary-search.ts
@@ -1,3 +1,13 @@
+/**
+ * 이진 탐색
+ * @param nums 정렬된 배열
+ * @param target 찾을 값
+ * @returns 찾은 값의 인덱스
+ *
+ * 시간복잡도: O(log n)
+ * 공간복잡도: O(1)
+ */
+
 export function search(nums: number[], target: number): number {
   let left = 0;
   let right = nums.length - 1;
